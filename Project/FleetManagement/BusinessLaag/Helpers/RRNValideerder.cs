4548479acbd2d128c1f104f10e0db0f0f69551fb
abdellahakhayad <abdellah.akhayad@student.hogent.be>
2021-10-14 13:48:52 +0200

    RRNValideerder

--- Project/FleetManagement/BusinessLaag/Helpers/RRNValideerder.cs
+++ Project/FleetManagement/BusinessLaag/Helpers/RRNValideerder.cs
@@ -1,23 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BusinessLaag.Exceptions;
-
+using System.Globalization;
 namespace BusinessLaag.Helpers
 {
     public class RRNValideerder
     {
-        public string valideer(string ongevalideerdRijksregisternummer)
+        public RRNValideerder(string rijksregisternummer)
         {
-            // indien successvol:
-            // return GevalideerdRRN
-            return ongevalideerdRijksregisternummer;
-            throw new NotImplementedException();
+            Valideer(rijksregisternummer);
+        }
+        public string Rijksregisternummer { get; set; }
+        public bool Valideer(string rijksregnummer)
+        {
+            Rijksregisternummer = rijksregnummer;
+            int geboortejaar = int.Parse(rijksregnummer.Substring(0, 2));
+            int geboorteMaand = int.Parse(rijksregnummer.Substring(2, 2));
+            int geboortedag = int.Parse(rijksregnummer.Substring(4, 2));
+            int e = CultureInfo.CurrentUICulture.Calendar.ToFourDigitYear(geboortejaar);
+            DateTime a = new DateTime(e, geboorteMaand, geboortedag);
+            //  string format = "dd/MM/yyyy";
+            int Controlecijfer = 0;
+            // Console.WriteLine(a.ToString(format));
+            if (a.Year >= 2000)
+            {
+                int divide = 97 - (int.Parse("2" + rijksregnummer.Substring(0, 9)) % 97);
+                Console.WriteLine(divide);
+                Controlecijfer += divide;
+            }
+            else
+            {
+                int divide = 97 - (int.Parse(rijksregnummer.Substring(0, 9)) % 97);
+                //  Console.WriteLine(divide);
+                Controlecijfer += divide;
+            }
+            if (Controlecijfer == int.Parse(rijksregnummer.Substring(9, 2)))
+            {
+                // ("Rijksregnummer geldig");
+                return true;
 
-            // indien niet successvol:
-            // throw new BestuurderException("Het opgegeven rijksregisternummer is ongeldig.");
+            }
+            else
+            {
+                // ("Rijksregnummer ongeldig");
+
+                throw new BestuurderException("rijksregisternummer is niet geldig");
+            }
+
         }
     }
-}
+    }
