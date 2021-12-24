399d81fa62a875d13a7e6c5e4ddf5602530647a3
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2021-12-24 12:56:12 +0100

    Update Serieel.sql

--- Ontwerp & analyse/SQL/Mock data/Serieel.sql
+++ Ontwerp & analyse/SQL/Mock data/Serieel.sql
@@ -10,7 +10,7 @@ DBCC CHECKIDENT ('FleetManager.dbo.TankkaartBrandstof',RESEED, 0)
 DELETE FROM Voertuig;
 DBCC CHECKIDENT ('FleetManager.dbo.Voertuig',RESEED, 0)
 
--- op het einde een update van rijksregisternummers, merk
+-- op het einde diverse updates zodat de data kan werken binnen applicatie (ivm model restricties)
 
 insert into FleetManager.dbo.Adres (Straatnaam, Huisnummer, Postcode, Plaatsnaam, Provincie, Land) values ('Birchwood', 44, '1170', 'Bruxelles', 'BRU', 'Belgium');
 insert into FleetManager.dbo.Adres (Straatnaam, Huisnummer, Postcode, Plaatsnaam, Provincie, Land) values ('Anzinger', 17, '1170', 'Bruxelles', 'BRU', 'Belgium');
